67cbd5a238c0aa1188c3cdda44a56c318ede26be
Matt Marchio <matt.marchio@gmail.com>
2025-03-05 23:22:55 -0800

    updating errors context functions

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -5,15 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime/debug"
+	"inventory/src/util"
 )
 
 var logger = log.New(os.Stdout, "\n\n", log.LstdFlags | log.Lshortfile)
+var ckey util.CtxKey = "stack"
+var ukey util.CtxKey = "updateCtx"
 
 type Error struct {
 	Message string `json:"message"`
 	Input interface{} `json:"input"`
-	Trace string `json:"trace"`
+	Trace []string `json:"trace"`
 	RequestUri string `json:"requestUri"`
 	Package string `json:"package"`
 	Function string `json:"function"`
@@ -22,14 +24,15 @@ type Error struct {
 }
 
 func (c Error) Err(ctx context.Context, e error) error {
-    if v, ok := ctx.Value("updateCtx").(func(context.Context, string, string) context.Context); ok {
-        ctx = v(ctx, "stack", "errors:errors.go:Err")
+    if v, ok := ctx.Value(ukey).(func(context.Context, util.CtxKey, string) context.Context); ok {
+        ctx = v(ctx, ckey, "errors:errors.go:Err")
     }
 	if c.Error != nil {
 		c.Error = e
 		c.Message = c.Error.Error()
-		c.Trace = string(debug.Stack())
-	
+		if v, ok := ctx.Value(ckey).([]string); ok {
+			c.Trace = v
+		}
 		logger.Printf("\n%#v\n", c)
 		return c.Error
 	}
@@ -37,13 +40,15 @@ func (c Error) Err(ctx context.Context, e error) error {
 }
 
 func (c Error) ErrOrNil(ctx context.Context, ptr interface{}, e error) error {
-    if v, ok := ctx.Value("updateCtx").(func(context.Context, string, string) context.Context); ok {
-        ctx = v(ctx, "stack", "errors:error.go:Error:ErrOrNil")
+    if v, ok := ctx.Value(ukey).(func(context.Context, util.CtxKey, string) context.Context); ok {
+        ctx = v(ctx, ckey, "errors:error.go:Error:ErrOrNil")
     }
 	if ptr == nil {
 		c.Error = fmt.Errorf("pointer is nil")
 		c.Message = c.Error.Error()
-		c.Trace = string(debug.Stack())
+		if v, ok := ctx.Value(ckey).([]string); ok {
+			c.Trace = v
+		}
 		logger.Printf("\n%#v\n", c)
 		return c.Error
 	}
@@ -54,8 +59,8 @@ func (c Error) ErrOrNil(ctx context.Context, ptr interface{}, e error) error {
 }
 
 func ErrOrNil(ctx context.Context, ptr interface{}, e error) error {
-    if v, ok := ctx.Value("updateCtx").(func(context.Context, string, string) context.Context); ok {
-        ctx = v(ctx, "stack", "errors:error.go:ErrOrNil")
+    if v, ok := ctx.Value(ukey).(func(context.Context, util.CtxKey, string) context.Context); ok {
+        ctx = v(ctx, ckey, "errors:error.go:ErrOrNil")
     }
 	err := Error{}
 	return err.ErrOrNil(ctx, ptr, e)	
